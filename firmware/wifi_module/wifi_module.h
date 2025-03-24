29fa147455c7f3300d002b3476a85ecb36e148f9
Luka Zeljko <luka@lukazeljko.xyz>
2025-03-24 13:02:53 +0100

    Added comments on wifi module functions

--- firmware/wifi_module/wifi_module.h
+++ firmware/wifi_module/wifi_module.h
@@ -3,8 +3,19 @@
 
 #include "../inc/inc.h"
 
+/**
+ * @brief initialise wifi module
+ *
+ * Function called only one time at the start of the program
+ */
 void wifi_init();
 
+/**
+ * @brief send character to wifi module
+ *
+ * Uses UART protocol to send a signle character to wifi module
+ * @param[in] character we want to send
+ */
 void wifi_send_character(uint8_t character);
 
 #endif
