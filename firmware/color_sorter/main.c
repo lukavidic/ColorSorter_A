0defb1ff1130465913fea873512a054f370d27cb
Luka Zeljko <luka@lukazeljko.xyz>
2025-03-27 10:56:37 +0100

    Changed Clock frequency to 32MHz => FCY=16MHz

--- firmware/color_sorter/main.c
+++ firmware/color_sorter/main.c
@@ -1,11 +1,10 @@
-#include "../inc/inc.h"
 #include "../servo/servo.h"
 #include "../color_sensor/color_sensor.h"
 #include "../led/led.h"
 #include "../wifi_module/wifi_module.h"
 #include "../button/button.h"
 
-#pragma config FNOSC = FRC    // Interni oscilator
+#pragma config FNOSC = FRCPLL    // Interni oscilator
 #pragma config JTAGEN = OFF   // IskljuCen JTAG
 #pragma config FWDTEN = OFF   // IskljuCen Watchdog timer
 
@@ -18,41 +17,10 @@ int main() {
 	wifi_init();
 	led_init();
     led_on();
-    __delay_ms(1000);
-	i2c_init();
-    led_off();
-    __delay_ms(1000);
-    
-    i2c_send_data(0x00);
-    
-    uint8_t redv[2];
-    uint8_t greenv[2];
-    uint8_t bluev[2];
 
-	uint16_t red;
-	uint16_t green;
-	uint16_t blue;
-    while(1);
-
-	/* while(1) { */
-	/* 	i2c_send_data(0x05); */
-	/* 	__delay_ms(100); */
-	/* 	i2c_get_data(&redv); */
-	/* 	__delay_ms(100); */
-	/* 	i2c_send_data(0x06); */
-	/* 	__delay_ms(100); */
-	/* 	i2c_get_data(&greenv); */
-	/* 	__delay_ms(100); */
-	/* 	i2c_send_data(0x07); */
-	/* 	__delay_ms(100); */
-	/* 	i2c_get_data(&bluev); */
-	/* 	__delay_ms(100); */
-	/* 	red = (redv[0] << 8) | redv[1]; */
-	/* 	green = (greenv[0] << 8) | greenv[1]; */
-	/* 	blue = (bluev[0] << 8) | bluev[1]; */
-	/* 	if(red > green && red > blue) */
-	/* 		led_on(); */
-	/* } */
+	while(1) {
+		
+	}
 }
 
 void pins_init() {
--- firmware/inc/inc.h
+++ firmware/inc/inc.h
@@ -2,7 +2,7 @@
 #define INC
 
 #include "xc.h"
-#define FCY 4000000UL
+#define FCY 16000000UL
 #define PRESCALER 256
 
 #include <libpic30.h>
