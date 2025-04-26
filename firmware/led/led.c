55575dbcf3780134a58b4ae626c59d77a3ecd1e5
Luka Zeljko <luka@lukazeljko.xyz>
2025-04-26 19:50:43 +0200

    Example of documentation

--- firmware/led/led.c
+++ firmware/led/led.c
@@ -1,13 +1,16 @@
 #include "led.h"
 
 void led_init() {
+	/* Sets LED pin as output */
 	TRISAbits.TRISA2 = 0;
 }
 
 void led_on() {
+	/* Sets LED state as high */
 	LATAbits.LATA2 = 1;
 }
 
 void led_off() {
+	/* Sets LED state as low */
 	LATAbits.LATA2 = 0;
 }
--- firmware/led/led.h
+++ firmware/led/led.h
@@ -1,12 +1,33 @@
+/**
+ * @file led.h
+ * @brief LED header file
+ *
+ * This file contains function definitions for LED control
+ */
+
 #ifndef LED_H
 #define LED_H
 
 #include "../inc/inc.h"
 
+/**
+ * Initializes LED module
+ *
+ * Takes no parameters and returns void
+ */
 void led_init();
 
+/**
+ * Turns LED ON
+ *
+ * Takes no parameters and returns void
+ */
 void led_on();
 
+/** Turns LED OFF
+ *
+ * Takes no parameters and returns void
+ */
 void led_off();
 
 #endif
