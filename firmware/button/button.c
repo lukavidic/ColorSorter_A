8db7bcd07765072b55de1608b48e8b5531ce24f2
AndjelaS997 <andjela.savicic@student.etf.unibl.org>
2025-05-07 21:06:59 +0200

    Dokumentacija za button.

--- firmware/button/button.c
+++ firmware/button/button.c
@@ -3,30 +3,24 @@
 #include "../servo/servo.h"
 #include "../color_sensor/color_sensor.h"
 #include "../inc/inc.h"
-//#define BUTTON PORTBbits.RB5
-//#define LED LATAbits.LATA2
-//int reset_counter=1;        //ako je reset_couter=1 -> zautsvi sistem , reset_counetr=2 -> pokreni sistem
+
 int reset_counter = 1;
 volatile int press_counter = 1;
 
 void button_init() {
     
-    TRISBbits.TRISB5 = 1;  // RB5 -> Dugme (ulaz)
+    TRISBbits.TRISB5 = 1;  // RB5 ->input
     initPrekidi();
 }
     
 void initPrekidi() {
-    /*__builtin_write_OSCCONL(OSCCON & ~(1 << 6));    // Otkljucavanje PPS registra
-    RPINR0bits.INT1R = 5;                           // Mapiranje RB5 (RP5) na INT1
-    __builtin_write_OSCCONL(OSCCON | (1 << 6));     // Zakljucavanje PPS registra*/
-
-    INTCON2bits.INT1EP = 1;                         // 1 = Silazna ivica (prekid pri pritisku dugmeta)
     
-    IFS1bits.INT1IF = 0;                            // Ocisti INT1 zastavicu prekida
-    IEC1bits.INT1IE = 1;                            // Omoguci INT1 prekid
-    IPC5bits.INT1IP = 7;                            // Postavi prioritet INT1 prekida (najvici)
 
-    INTCON2bits.GIE = 1;                            // Omoguci globalne prekide
+    INTCON2bits.INT1EP = 1;                         // 1 = Falling edge (interrupt on button press)
+    
+    IFS1bits.INT1IF = 0;                            
+
+    INTCON2bits.GIE = 1;                            
 }
 
 void __attribute__((interrupt, no_auto_psv)) _INT1Interrupt(void) {
@@ -38,12 +32,12 @@ void __attribute__((interrupt, no_auto_psv)) _INT1Interrupt(void) {
         led_off();
         servo_center();
         WS2812_SetColor(0, 0, 0);
-        //i2c_write2ByteRegister(VEML3328_SLAVE_ADD, CONF, 0x8011);
+        
         reset_counter = 0;
     } else {
         led_on();
         WS2812_SetColor(128, 128, 128);
-        //i2c_write2ByteRegister(VEML3328_SLAVE_ADD, CONF, 0x0400);
+        
         reset_counter = 1;
     }
     __delay_ms(300); 
--- firmware/button/button.h
+++ firmware/button/button.h
@@ -1,14 +1,42 @@
+/**
+ * @file button.h
+ * @brief BUTTON header file
+ *
+ * This file contains function definitions for BUTTON control
+ */
 #ifndef BUTTON_H
 
 #define BUTTON_H
 
 #include "../inc/inc.h"
+
+// External variable that counts button presses.
 extern volatile int press_counter;
 
+// External variable that controls system reset.
 extern int reset_counter;
 
+/**
+ * @brief Initializes the button and sets up interrupt configuration.
+ * 
+ * This function configures pin RB5 as an input and calls the function 
+ * to initialize interrupts.
+ */
 void button_init();
+/**
+ * @brief Initializes interrupts for the button.
+ * 
+ * This function sets up the parameters for INT1 interrupt, enables the interrupt 
+ * on falling edge, and enables global interrupts.
+ */
 void initPrekidi();
+/**
+ * @brief INT1 interrupt handler (button press).
+ * 
+ * This function is called when the button connected to INT1 is pressed. 
+ * It increments the press counter and toggles the system state (turns LED on/off, 
+ * centers the servo, and sets RGB color) based on the number of presses.
+ */
 void __attribute__((interrupt, no_auto_psv)) _INT1Interrupt(void);
 
 #endif
